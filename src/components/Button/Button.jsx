bf86fedc52ce530f320944d33d0674acc755f889
Evgeny Polyakov <polevg2015@yandex.ru>
2024-10-10 00:24:54 +0300

    refactor (Button.jsx), feature (JournalForm.jsx) create data processing form

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,9 @@
 import './Button.css';
-import { useState } from 'react';
 
-function Button() {
-  const [text, setText] = useState('Сохранить');
-
-  const clicked = () => {
-    text != 'Закрыть' ? setText('Закрыть') : setText('Сохранить');
-  }
+function Button({ text }) {
 
   return (
-    <button onClick={clicked} className="rounded-md py-2 px-[30px] 
+    <button className="rounded-md py-2 px-[30px] 
     font-inherit cursor-pointer transition-background duration-250 
     text-base border-none bg-btn hover:bg-[#222096]">{text}</button>
   );
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -8,14 +8,25 @@ function JournalForm(){
       setInputData(event.target.value);
       console.log(inputData);
     }
-  
+    
+    const addJournalItem = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formProps = Object.fromEntries(formData);
+        console.log(formProps);
+    }
+
     return (
         <>
-        <form className="journal-form flex gap-[30px] flex-col"></form>
-            <input type="date"/>
-            <input type="text" onChange={inputChange} value={inputData}/>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <Button/>
+        <form className="journal-form flex gap-[30px] flex-col" onSubmit={addJournalItem}>
+            <input type="title" name="title"/>
+            <input type="date" name="date"/>
+            <input type="text" name="tag" onChange={inputChange} value={inputData}/>
+            <textarea name="post" id="" cols="30" rows="10"></textarea>
+            <Button
+            text="Сохранить"
+            />
+        </form>
         </>
     );
 }
